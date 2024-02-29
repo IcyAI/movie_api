4fa01cdc4388106e86c88733768d642345a9bf05
IcyAI <alvinob1515@gmail.com>
2024-02-28 20:44:21 -0500

    Exercise 10

--- index.js
+++ index.js
@@ -17,23 +17,23 @@ const express = require('express'),
 const app = express();
 const { check, validationResult } = require('express-validator');
 
+
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 bodyParser = require('body-parser'),
 uuid = require('uuid');
-
-// const cors = require('cors');
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
